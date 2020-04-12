0219b68fcb6429b1c427c77dcceafe4ae36bc201
unknown <jake.m.brehm@gmail.com>
2020-04-11 22:52:47 -0400

    Minor changes and began creating tests.

---
+++ tests/test_launcher.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+'''
+multitooner.launcher unit test
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Unit test of the launcher module of Multitooner.
+'''
+
+import unittest
+
+import multitooner.launcher
+
+
+class TestAnalyze(unittest.TestCase):
+    '''Unit test the analyze module.'''
+
+    def test_failed_connection_due_to_incorrect_login(self):
+        '''Tests the ToontownLauncher._connect method in the failure
+        case due to incorrect login credentials.'''
+
+        # Initialize a ToontownLauncher object with a dummy install directory
+        launcher = multitooner.launcher.ToontownLauncher('')
+        # launcher._connect(username='username', password='password')
+        response = launcher._make_request({
+            'username': 'test', 
+            'password': 'test',
+        })
+        expected = {
+            'success': 'false',
+            'banner': 'Incorrect username and/or password.',
+        }
+        self.assertEqual(response, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
